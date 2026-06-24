7151e331a7371ac520d0cdf310c91daa2fd5a9f0
Murilo <delaramartinsmurilo@gmail.com>
2026-06-24 09:41:56 -0300

    Add files via upload

---
+++ Local_Market/Local_Market/sesiLocalMarket04/src/Carrinho/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+    //recupera carrinho do localstorage
+    const carrinho = JSON.parse(localStorage.getItem("arrinho")) || []
+    const listaElement = $("#lista")
+    const totalElement = $("#total")
+    function exibircarrinho(){
+        listaElement.empty()
+        let totalPreco = 0
+
+        $.each(carrinho, function(index, item){
+            const listaItem = $("<li>").text(`${item.desc} - preco: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("X").css("margin-left", "10px").click(function(){
+                
+                    removerItem(index)
+                
+               })
+                listaItem.append(removeButton)
+                listaElement.append(listItem)
+
+                totalPreco += item.preco
+        })
+        totalElement.text(`total: $${totalPreco.toFixed(2)}`)
+    }
+
+    function removerItem(index){
+        carrinho.splice(index,1)
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+        exibirCarringo()
+    }
+    exibirCarrinho()
+})
+function gerar(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listaElemente.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+    const totalHTML = totalElement.innerHTML
+    const conteudoHtml = `
+    <html>
+    
+    <head>
+
+    <meta charset="UTF-8">
+
+    </head>
+    <body>
+    <h1>PEDIDO CONFIRMADO</h1>
+    <h3>agradecemos sua compra e sua preferencia</h3>
+    <br>
+    ${listaHtml}
+    <br>
+    <br>
+    ${totalHtml}
+    </body>
+    </html>
+    `
+    
+    const blob = new Blob([contudoHTML], {type: "application/msword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.doc"
+    link.click()
+
+    document.getElementById("pedido").style.display = "block"
+}
+
+function successClose(){
+    document.getElementById("pedido").style.display = "none"
+}
