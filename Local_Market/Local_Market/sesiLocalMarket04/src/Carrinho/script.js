3bd77e5329497bdaec73f2e177a62994fce749d6
Murilo <delaramartinsmurilo@gmail.com>
2026-06-24 21:48:22 -0300

    Update script.js

--- Local_Market/Local_Market/sesiLocalMarket04/src/Carrinho/script.js
+++ Local_Market/Local_Market/sesiLocalMarket04/src/Carrinho/script.js
@@ -70,3 +70,79 @@ function gerar(){
 function successClose(){
     document.getElementById("pedido").style.display = "none"
 }
+
+
+
+
+
+teste esse 
+
+$(document).ready(function() {
+    //recupera carrinho do localstorage
+    const carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+    const listaElement = $("#lista")
+    const totalElement = $("#total")
+    function exibircarrinho(){
+        listaElement.empty()
+        let totalPreco = 0
+
+        $.each(carrinho, function(index, item){
+            const listaItem = $("<li>").text(`${item.desc} - preco: $${item.preco.toFixed(2)}`)
+
+            const removeButton = $("<button>").text("❌").css("margin-left", "10px").click(function(){
+                
+                    removerItem(index)
+                
+               })
+                listaItem.append(removeButton)
+                listaElement.append(listaItem)
+
+                totalPreco += item.preco
+        })
+        totalElement.text(`total: $${totalPreco.toFixed(2)}`)
+    }
+
+    function removerItem(index){
+        carrinho.splice(index,1)
+        localStorage.setItem("carrinho", JSON.stringify(carrinho))
+        exibirCarrinho()
+    }
+    exibirCarrinho()
+})
+function gerar(){const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+    const listaClone = listaElement.cloneNode(true)
+    $(listaClone).find("button").remove()
+    const listaHtml = listaClone.innerHTML
+    const totalhTML = totalElement.innerHTML
+    const conteudoHtml =`
+    <html>
+        <head>
+            <meta charSet="UTF-8"></meta>
+        </head>
+        <body>
+            <h1>PEDIDO CONFIRMADO</h1>
+            <h3>Agradecemos sua compra e sua preferencia.</h3>
+            <br>
+            ${listaHtml}
+            </br>
+            <br>
+            ${totalhTML}
+            </br>
+        </body>
+    </html>`
+
+    const blob = new Blob([conteudoHtml],{type: "application/msword"})
+    const link = document.createElement("a")
+
+    link.href = URL.createObjectURL(blob)
+    link.download = "pedido.doc"
+    link.click()
+    document.getElementById("pedido").style.display ="block"
+    
+}
+function successClose(){
+    document.getElementById("pedido").style.display = "none"
+}
+
+    
